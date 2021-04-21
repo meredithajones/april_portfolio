1b58217a4a26e8ef8c75964b0a03018b21ac7bf6
Meredith Jones <mj19003@gmail.com>
2021-04-21 08:12:04 -0400

    Finished nav dropdown javascript

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const mobileMenu = document.querySelector('#mobile-menu')
-const menuLinks =document.querySelector('navbar_menu')
+const menuLinks =document.querySelector('.navbar_menu')
 
 //Display Mobile Menu
-
+const openMenu = () => {
+    mobileMenu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+//Add an event listner to toggle
+mobileMenu.addEventListener('click', openMenu);
