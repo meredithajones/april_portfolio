5883875af89118d604deaa688df3e1d407807342
Meredith Jones <mj19003@gmail.com>
2021-04-21 14:31:00 -0400

    Setting up javascript to highlight current page on navbar

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const openMenu = () => {
 }
 //Add an event listner to toggle
 mobileMenu.addEventListener('click', openMenu);
+
+//Show Active menu when scrolling
+const highlightMenu = () => {
+    const homeNav = document.querySelector('#home_section')
+    const aboutNav = document.querySelector('.#about_section')
+    const portfolioNav = document.querySelector('#portfolio_section')
+    const contactNav = document.querySelector('#contact-section')
+    let scrollDisplay = window.scrollY
+    
+
+
+}
