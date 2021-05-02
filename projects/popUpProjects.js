6752cc2def2209a32f97d40d047e00d3a0a378a1
Meredith Jones <mj19003@gmail.com>
2021-05-02 08:05:31 -0400

    Added Javascript selecting all the elements in the dom that will be used in card animations

--- projects/popUpProjects.js
+++ projects/popUpProjects.js
@@ -11,3 +11,15 @@ function init() {
   selectElements();
   attachListeners();
 }
+
+// Selecting all the elements in the DOM that are going to be used
+function selectElements() {
+    cards = document.getElementsByClassName('card'),
+    nCards = cards.length,
+    cover = document.getElementById('cover'),
+    openContent = document.getElementById('open-content'),
+    openContentText = document.getElementById('open-content-text'),
+    openContentImage = document.getElementById('open-content-image')
+    closeContent = document.getElementById('close-content');
+  }
+  
