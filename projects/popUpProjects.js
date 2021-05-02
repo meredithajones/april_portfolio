c06b1c23ca4281dfb7e0ccf7521a180ee2b5be43
Meredith Jones <mj19003@gmail.com>
2021-05-02 08:07:54 -0400

    Added event listners for when card is clicked, when close icon is clicked, and to resze window

--- projects/popUpProjects.js
+++ projects/popUpProjects.js
@@ -22,4 +22,38 @@ function selectElements() {
     openContentImage = document.getElementById('open-content-image')
     closeContent = document.getElementById('close-content');
   }
+
+  /* Attaching three event listeners:
+  - a click event listener for each card
+  - a click event listener to the close button
+  - a resize event listener on the window
+*/
+function attachListeners() {
+    for (var i = 0; i < nCards; i++) {
+      attachListenerToCard(i);
+    }
+    closeContent.addEventListener('click', onCloseClick);
+    window.addEventListener('resize', resize);
+  }
+  
+  function attachListenerToCard(i) {
+    cards[i].addEventListener('click', function(e) {
+      var card = getCardElement(e.target);
+      onCardClick(card, i);
+    })
+  }
+  
+  /* When a card is clicked */
+function onCardClick(card, i) {
+    // set the current card
+    currentCard = card;
+    // add the 'clicked' class to the card, so it animates out
+    currentCard.className += ' clicked';
+    // animate the card 'cover' after a 500ms delay
+    setTimeout(function() {animateCoverUp(currentCard)}, 500);
+    // animate out the other cards
+    animateOtherCards(currentCard, true);
+    // add the open class to the page content
+    openContent.className += ' open';
+  }
   
